e2101b5490725c0ef0355812fbb674b478874929
Mongkol Tuntisukumarl <mingbiz@gmail.com>
2026-07-06 17:21:31 +0700

    Third version of DBS

--- query4.mongodb.js
+++ query4.mongodb.js
@@ -1,4 +1,4 @@
 use("sample_mflix");
 db.movies.find({ plot: { $regex: "American", $options: "i" } }).count()
 db.movies.find({ plot: { $regex: "street.$", $options: "i" } }).count()
-db.movies.find({ plot: { $regex: "street.$", $options: "i" } })
+db.movies.find({ plot: { $regex: "street.$", $options: "i" } });
---
+++ query5.mongodb.js
@@ -0,0 +1,6 @@
+use("sample_mflix");
+db.movies.find({}).sort({ runtime: -1 }).limit(5);
+db.movies.find({ runtime: { $lt: 60 } }).sort({ runtime: -1 }).limit(5);
+db.movies.find({ released: { $gte: ISODate("1990-01-01T00:00:00Z"), $lt: ISODate("2001-01-01T00:00:00Z") } }).count();
+
+
